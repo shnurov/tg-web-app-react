bed3861672b50fbe40007387b4a3e84b07de4ed6
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-05-22 21:27:42 +0200

    feat: add products

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,22 +1,24 @@
 const tg = window.Telegram.WebApp;
 
-export function useTelegram () {
-  const onClose = () => {
-    tg.close()
-  }
-  const onToggleButton = () => {
-    if(tg.MainButton.isVisible) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
+export function useTelegram() {
+
+    const onClose = () => {
+        tg.close()
     }
-  }
-  return {
-    onClose,
-    onToggleButton,
-    tg,
-    user: tg.initDataUnsafe?.user,
-  }
+
+    const onToggleButton = () => {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
+    }
 }
-
-
