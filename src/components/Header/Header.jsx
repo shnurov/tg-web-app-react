c06f571edb6859d4d462c541b3c4513517e88d20
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-05-22 20:50:19 +0200

    feat: add hooks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Button from '../Button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = () => {
-  const tg = window.Telegram.WebApp;
+  const {user, onClose} = useTelegram();
 
-  const onClose = () => {
-    tg.close()
-  }
 
   return (
     <div className={'header'}>
